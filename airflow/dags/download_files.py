1f4d81a27cdd5c6efdf04d30866636914bc57b70
nshuman-ucb <89490653+nshuman-ucb@users.noreply.github.com>
2022-08-02 13:36:15 -0400

    Updated doc strings and comments.

---
+++ airflow/dags/download_files.py
@@ -0,0 +1,35 @@
+'''download_files.py
+Unpacks a dictionary containing a list which houses a file description, date in YYYY-MM format, file extension,
+and a url to download a file. Downloads files using urllib library and pushes a dictionary as a XCOM
+variable to be used to upload the files to a remote destination.
+'''
+def download_files(ti):
+    '''
+    Reads a dictionary containing a subject as a key, and a list stored as the value pair containing the following:
+        list[0] = ******@@@@@*****  -- TO-DO: populate this description based on whether this function will be for NY taxi only
+        list[1] = YYYY-MM
+        list[2] = file extension
+        list[4] = url to download file
+    Utilizes unpacked list values to set a path and filename before downloading the files and saving them to the local machine.
+
+    '''
+
+    
+    download_info = ti.xcom_pull(key='download_info', task_ids='get_url')
+    source_dest_gcs = defaultdict()    
+
+    for k,v in download_info.items():
+        taxi_type = v[0]
+        year_month = v[1]
+        file_ext = v[2]
+        url = v[3]
+
+        parent_dir = os.path.abspath(os.path.join(os.path.join(os.path.dirname(__file__), '..'), taxi_type))
+        file_name = f'{taxi_type}_{year_month}{file_ext}'
+        dest = os.path.join(parent_dir, file_name)
+        os.makedirs(parent_dir, exist_ok=True)
+            
+        urllib.request.urlretrieve(url, dest)
+        source_dest_gcs[file_name] = { 'source' : dest, 'dest' : f'{taxi_type}/{taxi_type}_{year_month}{file_ext}' }
+
+    ti.xcom_push(key='gcs_path', value = source_dest_gcs)
--- airflow/dags/nyc_taxi_dag.py
+++ airflow/dags/nyc_taxi_dag.py
@@ -2,17 +2,16 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
-from random import uniform
 from datetime import datetime
-import os
-import urllib.request
+
 
 from get_urls import get_urls
 from upload_to_gcs import process_xcom_for_gcs_upload
 from upload_to_gcs import upload_blob
 from get_weather import get_weather_data
+from download_files import download_files
 
-from collections import defaultdict
+
 
 
 
@@ -23,62 +22,10 @@ default_args = {
     "retries": 1,
 }
 
+# Variables
+
 LINK = "https://www1.nyc.gov/site/tlc/about/tlc-trip-record-data.page"
-BUCKET_NAME = 'levant-data-lake_nyc-taxi-dwh'
-
-# def _get_urls(ti, url, year, month):
-#     urls = []
-#     page = requests.get(url)
-#     soup = BeautifulSoup(page.text, 'html.parser')
-
-#     for link in soup.find_all('a'):
-#         urls.append(link.get('href'))
-
-#     dataset_regex = re.compile('.tripdata_' + year + '-' + month + '.[a-z]*')
-#     url_regex = re.compile('([a-z]*_[a-z]*)_(\d{4}-\d{2})(.parquet)')
-
-#     links=list(filter(dataset_regex.search, urls))
-#     to_pass = defaultdict()
-
-#     for download_url in links:
-#         result = re.search(url_regex, download_url)
-#         dir, date, ext = result.groups()
-#         to_pass[dir] = [dir, date, ext, download_url]
-
-#     ti.xcom_push(key='download_info', value = to_pass)
-        
-
-def _download_files(ti):
-    '''
-    Reads a dictionary containing a subject as a key, and a list stored as the value pair containing the following:
-        list[0] = ******@@@@@*****  -- TO-DO: populate this description based on whether this function will be for NY taxi only
-        list[1] = YYYY-MM
-        list[2] = file extension
-        list[4] = url to download file
-    Utilizes unpacked list values to set a path and filename before downloading the files and saving them to the local machine.
-
-    '''
-
-    
-    download_info = ti.xcom_pull(key='download_info', task_ids='get_url')
-    source_dest_gcs = defaultdict()    
-
-    for k,v in download_info.items():
-        taxi_type = v[0]
-        year_month = v[1]
-        file_ext = v[2]
-        url = v[3]
-
-        parent_dir = os.path.abspath(os.path.join(os.path.join(os.path.dirname(__file__), '..'), taxi_type))
-        file_name = f'{taxi_type}_{year_month}{file_ext}'
-        dest = os.path.join(parent_dir, file_name)
-        os.makedirs(parent_dir, exist_ok=True)
-            
-        urllib.request.urlretrieve(url, dest)
-        source_dest_gcs[file_name] = { 'source' : dest, 'dest' : f'{taxi_type}/{taxi_type}_{year_month}{file_ext}' }
-
-    ti.xcom_push(key='gcs_path', value = source_dest_gcs)
-
+BUCKET_NAME = 'levant-data-lake_nyc-taxi-dwh' 
 
 
 with DAG('nyc_taxi_dag', 
@@ -104,9 +51,9 @@ with DAG('nyc_taxi_dag',
     )
      
 
-    download_files = PythonOperator(
+    download_from_url = PythonOperator(
         task_id='download_files',
-        python_callable=_download_files
+        python_callable=download_files
     )
 
     local_to_gcs = PythonOperator(
@@ -136,7 +83,7 @@ with DAG('nyc_taxi_dag',
         }
     )
 
-    downloading_data >> get_url >> download_files >> local_to_gcs
+    downloading_data >> get_url >> download_from_url >> local_to_gcs
 
     get_weather >> upload_weather
     
