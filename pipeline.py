0ec3da939eeae21bc2de47457e554d3f1b96e537
Najee Shuman <nshuman@berkeley.edu>
2022-08-01 23:23:02 -0400

    folder cleanup

--- pipeline.py
+++
@@ -1,8 +0,0 @@
-import pyarrow.parquet as pq
-
-
-trips = pq.read_table('yellow_tripdata_2022-01.parquet')
-trips = trips.to_pandas()
-
-
-print(f'Finished succesfully, data shape is: {trips.shape}')
